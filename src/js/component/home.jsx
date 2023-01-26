9bf9f75bcb0d3663d7fa706e776b7353601a6cf6
BrodyDubach <brodydubach@gmail.com>
2023-01-26 22:15:39 +0000

    fixes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,71 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import Toggle from "./Toggle";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+//parent component
+function Home() {
+  const [task, setTask] = useState("");
+  const [variable, setVariable] = useState([
+    "Do Homework",
+    "Do Laundry",
+    "Walk the Dog",
+  ]);
+
+  let todo = variable.map((item, i) => {
+    return (
+      <li className="list-group-item item" key={i}>
+        {item}
+        <div
+          className="mouseOver"
+          style={{ float: "right" }}
+          onClick={() => removeItem(i)}
+        >
+          x
+        </div>
+      </li>
+    );
+  });
+
+  //Add Function
+  const newTodo = (onKeyDownEvent) => {
+    if (onKeyDownEvent.key === "Enter") {
+      const newTodo = [...variable, task];
+      setVariable(newTodo);
+      setTask("");
+    }
+  };
+
+  //Delete Function
+  const removeItem = (index) => {
+    const newArray = variable.filter((item, i) => i !== index);
+    setVariable(newArray);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="todo">Todos</h1>
+    <div className="main"> 
+      <input
+        className="input"
+        placeholder= "Add your task here"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        type="text"
+        id="fname"
+        name="fname"
+        onKeyDown={newTodo}
+      />
+    <div>
+		  <ul className="task">{todo}</ul>
+		  <div className="remaining">{todo.length} items left</div>
 		</div>
-	);
-};
+      <p>
+        {/* <Toggle /> */}
+      </p>
+    </div>
+    </div>
+  );
+}
+
 
 export default Home;
